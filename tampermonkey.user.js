5daa957133d3e937f02a7040535bd38277f5d217
T. Dũng <85283786+phantatdung233@users.noreply.github.com>
2025-04-20 21:35:04 +0700

    Create: tampermonkey.user.js

---
+++ tampermonkey.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Tool UGPhone
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Tool UGPhone
+// @author       xGreen
+// @match        https://www.ugphone.com/*
+// @require      https://raw.githubusercontent.com/phantatdung233/auto-ugphone/refs/heads/main/auto-ugphone-v1.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @run-at       document-end
+// @icon         https://avatars.githubusercontent.com/u/85283786?s=48&v=4
+// ==/UserScript==
